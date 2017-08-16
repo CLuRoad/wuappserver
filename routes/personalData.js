606dde3dadcb14e1bbe5c305c7680a58c358ae00
RoadClu <roadc@RoadCludeMacBook-Pro.local>
2017-08-16 09:59:55 +0800

    dsg

---
+++ routes/personalData.js
@@ -0,0 +1,22 @@
+module.exports = {
+  isSuccess: true,
+  code: 0001,
+  Message: "请求成功",
+  data: {
+    userId: "0141953",
+    userName: "陈涛",
+    email:'chentao@worldunion.com.cn', //邮箱
+    department: "深圳总部",
+    departmentlabel: "广州顾问及工商资产运营事业部_营业二部",//本部标签
+    channelabbreviation: "广州地区直辖", //渠道简称
+    channelCode:"1104-000-003-1071", //渠道编码
+    jobStation:"商业咨询顾问", //岗位
+    channelRole:"渠道推介成员",  //渠道角色
+    leader:"渠道推介成员",  //上级
+    cityName:'广州',
+    CityId:'009449',
+    telPhone: "13399999999",
+    region: "广州地区" , //归属地区
+    userPic:'http://facebook.github.io/react/img/logo_og.png'
+  }
+};
---
+++ routes/userInfo.js
@@ -0,0 +1,18 @@
+module.exports = {
+  isSuccess: true,
+  code: 0001,
+  Message: "请求成功",
+  data: {
+    userId: "0141953",
+    userName: "陈涛",
+    department: "深圳总部",
+    hApplicationFormNum: "18",
+    dApplicationFormNum: "20",
+    allIncome: "19000",
+    dealIncome: "8000",
+    resultsRank: "2",
+    learnNum: "10",
+    telPhone: "13399999999",
+    region: "广州地区直辖"
+  }
+};
