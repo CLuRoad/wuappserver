54ee0062b1ec9ac14e2e804a66059a8c75023c0e
RoadClu <roadc@RoadCludeMacBook-Pro.local>
2017-08-16 10:06:55 +0800

    sg

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// 返回简单的 JSON
+router.get('/getAll', function(req, res, next) {
+  
+    var data = {
+      "靳东":"贺涵", "马伊琍":"罗子君", "袁泉":"唐晶", "雷佳音":"陈俊生 ", "吴越":"凌玲", "许娣":"薛甄珠", "张龄心":"罗子群", "邬君梅":"吴大娘", "栾元晖":"白光", "郑罗茜":"薇薇安", "啜妮":"洛洛", "梅婷":"卓渐清前女友", "谭凯":"Adom", "孔维":"单身女人", "张棪琰":"苏曼殊", "侯岩松":"老金", "魏之皓":"平儿", "王天泽":"冷佳清", "于明加":"安琪儿CEO", "任东霖":"李睿", "张衣":"罗平", "黄澜":"M女士", "陈道明":"卓渐清"
+    }
+  
+    var response = {status:1,data:data};
+    res.send(JSON.stringify(response));
+  
+  });
+
+
 module.exports = router;
